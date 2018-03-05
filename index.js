55630b7bd9b37ad3efbf6df2ef926eb8f1359e7f
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-03-05 09:40:11 +0000

    Add BDD expect support

--- index.js
+++ index.js
@@ -22,5 +22,29 @@ module.exports = {
         assert.web3Events(observedTransactionResult, [expectedEvent], message);
       };
     }
+
+    if (expect !== undefined) {
+      expect.web3Events = function(observedTransactionResult, expectedEvents, message) {
+        let entries = observedTransactionResult.logs.map(function(logEntry) {
+          return {
+            event: logEntry.event,
+            args: Object.keys(logEntry.args).reduce(function(previous, current) {
+              previous[current] =
+                (typeof logEntry.args[current].toNumber === 'function')
+                  ? logEntry.args[current].toNumber()
+                  : logEntry.args[current];
+              return previous;
+            }, {})
+          }
+        });
+        // expect({a: 1}).to.deep.equal({a: 1});
+        // expect.deepEqual(entries, expectedEvents, message);
+        expect(entries).to.deep.equal(expectedEvents);
+      };
+
+      expect.web3Event = function(observedTransactionResult, expectedEvent, message) {
+        expect.web3Events(observedTransactionResult, [expectedEvent], message);
+      };
+    }
   }
 }
