88ad13facb4034b623decc133db61aa28c6273ee
Philippe Bernard <philippe.bernard@gmail.com>
2018-03-30 13:04:08 +0200

    Code refactored (DRY)

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 
+function buildObservedEventsForComparison(observedTransactionResult, expectedEvents) {
+  return observedTransactionResult.logs.map(function(logEntry) {
+    return {
+      event: logEntry.event,
+      args: Object.keys(logEntry.args).reduce(function(previous, current) {
+        previous[current] =
+          (typeof logEntry.args[current].toNumber === 'function')
+            ? logEntry.args[current].toNumber()
+            : logEntry.args[current];
+        return previous;
+      }, {})
+    }
+  });
+}
+
 module.exports = {
   init: function() {
     if (typeof assert !== 'undefined') {
       assert.web3Events = function(observedTransactionResult, expectedEvents, message) {
-        let entries = observedTransactionResult.logs.map(function(logEntry) {
-          return {
-            event: logEntry.event,
-            args: Object.keys(logEntry.args).reduce(function(previous, current) {
-              previous[current] =
-                (typeof logEntry.args[current].toNumber === 'function')
-                  ? logEntry.args[current].toNumber()
-                  : logEntry.args[current];
-              return previous;
-            }, {})
-          }
-        });
+        let entries = buildObservedEventsForComparison(observedTransactionResult, expectedEvents);
         assert.deepEqual(entries, expectedEvents, message);
       };
 
@@ -25,18 +29,7 @@ module.exports = {
 
     if (typeof expect !== 'undefined') {
       expect.web3Events = function(observedTransactionResult, expectedEvents, message) {
-        let entries = observedTransactionResult.logs.map(function(logEntry) {
-          return {
-            event: logEntry.event,
-            args: Object.keys(logEntry.args).reduce(function(previous, current) {
-              previous[current] =
-                (typeof logEntry.args[current].toNumber === 'function')
-                  ? logEntry.args[current].toNumber()
-                  : logEntry.args[current];
-              return previous;
-            }, {})
-          }
-        });
+        let entries = buildObservedEventsForComparison(observedTransactionResult, expectedEvents);
         // expect({a: 1}).to.deep.equal({a: 1});
         // expect.deepEqual(entries, expectedEvents, message);
         expect(entries).to.deep.equal(expectedEvents);
