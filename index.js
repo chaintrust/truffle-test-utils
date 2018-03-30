b5fe0f7fdb6a7618a4d6d7902f1ef88dcaeb9464
Philippe Bernard <philippe.bernard@gmail.com>
2018-03-30 15:19:14 +0200

    web3AllEvents and web3SomeEvents

--- index.js
+++ index.js
@@ -1,52 +1,72 @@
 
-function buildObservedEventsForComparison(observedTransactionResult, expectedEvents) {
-  return observedTransactionResult.logs.map(function(logEntry) {
-    // Event name
-    let event = {
-      event: logEntry.event
-    };
+function buildObservedEventsForComparison(observedTransactionResult, expectedEvents, filterByName) {
+  let observedEvents = new Array();
 
-    // Event arguments
+  observedTransactionResult.logs.forEach(function(logEntry) {
     let expectedEntry = expectedEvents.find(function(evt) {
       return (evt.event === logEntry.event)
     });
-    // Ignore the arguments when they are not tested
-    // (ie. expectedEntry.args is undefined)
-    if ((! expectedEntry) || (expectedEntry && expectedEntry.args)) {
-      event.args = Object.keys(logEntry.args).reduce(function(previous, current) {
-        previous[current] =
-          (typeof logEntry.args[current].toNumber === 'function')
-            ? logEntry.args[current].toNumber()
-            : logEntry.args[current];
-        return previous;
-      }, {});
-    }
 
-    return event;
+    // When filtering, ignore events that are not expected
+    if ((! filterByName) || expectedEntry) {
+      // Event name
+      let event = {
+        event: logEntry.event
+      };
+
+      // Event arguments
+      // Ignore the arguments when they are not tested
+      // (ie. expectedEntry.args is undefined)
+      if ((! expectedEntry) || (expectedEntry && expectedEntry.args)) {
+        event.args = Object.keys(logEntry.args).reduce(function(previous, current) {
+          previous[current] =
+            (typeof logEntry.args[current].toNumber === 'function')
+              ? logEntry.args[current].toNumber()
+              : logEntry.args[current];
+          return previous;
+        }, {});
+      }
+
+      observedEvents.push(event);
+    }
   });
+
+  return observedEvents;
 }
 
 module.exports = {
   init: function() {
     if (typeof assert !== 'undefined') {
-      assert.web3Events = function(observedTransactionResult, expectedEvents, message) {
-        let entries = buildObservedEventsForComparison(observedTransactionResult, expectedEvents);
+      assert.web3AllEvents = function(observedTransactionResult, expectedEvents, message) {
+        let entries = buildObservedEventsForComparison(observedTransactionResult, expectedEvents, false);
+        assert.deepEqual(entries, expectedEvents, message);
+      };
+      assert.web3Events = assert.web3AllEvents;
+
+      assert.web3SomeEvents = function(observedTransactionResult, expectedEvents, message) {
+        let entries = buildObservedEventsForComparison(observedTransactionResult, expectedEvents, true);
         assert.deepEqual(entries, expectedEvents, message);
       };
 
       assert.web3Event = function(observedTransactionResult, expectedEvent, message) {
-        assert.web3Events(observedTransactionResult, [expectedEvent], message);
+        assert.web3SomeEvents(observedTransactionResult, [expectedEvent], message);
       };
     }
 
     if (typeof expect !== 'undefined') {
-      expect.web3Events = function(observedTransactionResult, expectedEvents, message) {
-        let entries = buildObservedEventsForComparison(observedTransactionResult, expectedEvents);
+      expect.web3AllEvents = function(observedTransactionResult, expectedEvents, message) {
+        let entries = buildObservedEventsForComparison(observedTransactionResult, expectedEvents, false);
+        expect(entries).to.deep.equal(expectedEvents);
+      };
+      expect.web3Events = expect.web3AllEvents;
+
+      expect.web3SomeEvents = function(observedTransactionResult, expectedEvents, message) {
+        let entries = buildObservedEventsForComparison(observedTransactionResult, expectedEvents, true);
         expect(entries).to.deep.equal(expectedEvents);
       };
 
       expect.web3Event = function(observedTransactionResult, expectedEvent, message) {
-        expect.web3Events(observedTransactionResult, [expectedEvent], message);
+        expect.web3SomeEvents(observedTransactionResult, [expectedEvent], message);
       };
     }
   }
